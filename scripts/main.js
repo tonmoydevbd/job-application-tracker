94b119a7a4076db4196014679ee1127e1cdda7be
tonmoydevbd <tonmoydevbd@gmail.com>
2026-02-24 21:28:22 +0600

    added all functionality for card body button.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
-document.querySelectorAll('.total-jobs').forEach(item => item.innerText = jobsData.length);
 // filtered card data 
-const interviewCardData = [];
-const rejectedCardData = [];
+let interviewCardData = [];
+let rejectedCardData = [];
 
 // all variable 
+const totalJobsCountEl = document.querySelectorAll('.total-jobs');
+const interviewCountEl = document.querySelector('#interview-count');
+const rejectedCountEl = document.querySelector('#rejected-count');
 let sectionEl = document.querySelector('section');
 let noJobsSection = document.querySelector('.no-jobs-section');
 const toggleBtnContainer = document.querySelector('.toggle-btn-container')
@@ -12,12 +14,16 @@ const allCardBtn = toggleBtnContainer.children[0];
 const interviewBtn = toggleBtnContainer.children[1];
 const rejectedBtn = toggleBtnContainer.children[2];
 
+totalJobsCountEl.forEach(item => item.innerText = jobsData.length);
+interviewCountEl.innerText = interviewCardData.length;
+rejectedCountEl.innerText = rejectedCardData.length;
+
 function renderCard(data) {
   sectionEl.innerHTML = '';
   if (data.length) {
-    data.forEach(function (card, i) {
+    data.forEach(card => {
       sectionEl.innerHTML += `
-      <article class="card">
+      <article id="${card.id}" class="card">
         <div>
           <h3>${card.companyName}</h3>
           <p>${card.position}</p>
@@ -40,13 +46,12 @@ function renderCard(data) {
         </div>
       </article>
     `
-      noJobsSection.classList.remove('show');
+      noJobsSection.classList.add('hide');
     });
   } else {
-    noJobsSection.classList.add('show');
+    noJobsSection.classList.remove('hide');
   }
 }
-
 
 allCardBtn.addEventListener('click', () => {
   allCardBtn.classList.add('active-btn');
@@ -64,5 +69,52 @@ rejectedBtn.addEventListener('click', () => {
   rejectedBtn.classList.add('active-btn');
   allCardBtn.classList.remove('active-btn');
   interviewBtn.classList.remove('active-btn');
-  renderCard(rejectedCardData);
+  renderCard(rejectedCardData)
 });
+
+
+document.getElementById('card-section').addEventListener('click', (e) => {
+  const clickedBtn = e.target;
+  const targetCardId = Number(clickedBtn.parentElement.parentElement.parentElement.getAttribute('id'));
+
+  if (clickedBtn.innerText === 'REJECTED') {
+    jobsData.forEach(item => {
+      if (targetCardId === item.id) {
+        item.status = 'rejected';
+        rejectedCardData.push(item)
+        if (interviewCardData.length) {
+          interviewCardData = interviewCardData.filter(item => item.id !== targetCardId);
+        }
+      }
+    })
+    rejectedCardData = rejectedCardData.filter((item, index, self) => {
+      return self.indexOf(item) === index;
+    })
+    refreshView()
+
+  } else if (clickedBtn.innerText === 'INTERVIEW') {
+    jobsData.forEach(item => {
+      if (targetCardId === item.id) {
+        item.status = 'interview';
+        interviewCardData.push(item);
+        if (rejectedCardData.length) {
+          rejectedCardData = rejectedCardData.filter(item => item.id !== targetCardId);
+        }
+      }
+    })
+    interviewCardData = interviewCardData.filter((item, index, self) => {
+      return self.indexOf(item) === index;
+    })
+    refreshView()
+
+  }
+})
+function refreshView() {
+  if (allCardBtn.classList.contains('active-btn')) {
+    renderCard(jobsData)
+  } else if (interviewBtn.classList.contains('active-btn')) {
+    renderCard(interviewCardData)
+  } else if (rejectedBtn.classList.contains('active-btn')) {
+    renderCard(rejectedCardData)
+  }
+}
